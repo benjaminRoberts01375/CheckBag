9cdadbbe9331f6800c7f0824a3a451176f93c3d0
benjaminroberts01375 <benroberts01375@gmail.com>
2025-07-11 11:26:40 -0400

    Switch to 410 error if user does not exist

--- backend/userExists.go
+++ backend/userExists.go
@@ -9,8 +9,10 @@ import (
 func userExists(w http.ResponseWriter, r *http.Request) {
 	data, err := fileSystem.GetUserData()
 	if err != nil || data == "" {
-		Coms.ExternalPostRespondCode(http.StatusNotFound, w)
+		Coms.Println("User does not exist")
+		Coms.ExternalPostRespondCode(http.StatusGone, w)
 		return
 	}
+	Coms.Println("User exists")
 	Coms.ExternalPostRespondCode(http.StatusOK, w)
 }
