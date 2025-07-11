7b762b40a10809dc89b4313e078d9496c3070a5f
benjaminroberts01375 <benroberts01375@gmail.com>
2025-07-11 16:42:43 -0400

    Add buttons that change color based on route

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,11 +1,29 @@
 import "../styles.css";
 import NavbarStyles from "./navbar.module.css";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const Navbar = () => {
+	const navigate = useNavigate();
+	const location = useLocation();
+
 	return (
-		<nav id={NavbarStyles["navbar-container"]}>
-			<h1>Navbar</h1>
-		</nav>
+		<div id={NavbarStyles["navbar-container"]}>
+			<h1 id={NavbarStyles["title"]}>CheckBag</h1>
+			<div id={NavbarStyles["divider"]}></div>
+			<button
+				className={`${NavbarStyles["entry"]} ${location.pathname === "/dashboard/home" ? NavbarStyles["active"] : ""}`}
+				onClick={() => navigate("/dashboard/home")}
+			>
+				<p>Home</p>
+			</button>
+			<button
+				className={`${NavbarStyles["entry"]} ${location.pathname === "/dashboard/services" ? NavbarStyles["active"] : ""}`}
+				onClick={() => navigate("/dashboard/services")}
+			>
+				<p>Services</p>
+			</button>
+			<div id={NavbarStyles["divider"]}></div>
+		</div>
 	);
 };
 
