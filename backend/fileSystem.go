a87e7e2a3b82a9281d009172cc59fde4235baeab
benjaminroberts01375 <benroberts01375@gmail.com>
2025-07-12 17:48:54 -0400

    Enable setting services in backend

--- backend/fileSystem.go
+++ backend/fileSystem.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"os"
 	"path/filepath"
@@ -13,11 +14,13 @@ func init() {
 var fileSystem = FileSystem{
 	BasePath: "/CheckBag",
 	UserData: "userdata.txt",
+	Services: "services.json",
 }
 
 type FileSystem struct {
 	BasePath string `json:"base_path"`
 	UserData string `json:"user_data"`
+	Services string `json:"services"`
 }
 
 func (fs *FileSystem) Setup() {
@@ -40,7 +43,6 @@ func (fs *FileSystem) Write(path string, data string) error {
 }
 
 func (fs *FileSystem) GetUserData() (string, error) {
-	data, err := os.ReadFile(fs.GetUserDataPath())
 	data, err := os.ReadFile(filepath.Join(fs.BasePath, fs.UserData))
 	return string(data), err
 }
@@ -48,8 +50,24 @@ func (fs *FileSystem) GetUserData() (string, error) {
 func (fs *FileSystem) SetUserData(data string) error {
 	return fs.Write(filepath.Join(fs.BasePath, fs.UserData), data)
 }
+
+func (fs *FileSystem) SetServices(services ServiceLinks) error {
+	data, err := json.Marshal(services)
 	if err != nil {
-		return errors.New("Could not write to file: " + err.Error())
+		return errors.New("Could not marshal services: " + err.Error())
 	}
-	return nil
+	return fs.Write(filepath.Join(fs.BasePath, fs.Services), string(data))
+}
+
+func (fs *FileSystem) GetServices() (ServiceLinks, error) {
+	data, err := os.ReadFile(filepath.Join(fs.BasePath, fs.Services))
+	if err != nil {
+		return ServiceLinks{}, errors.New("Could not read services: " + err.Error())
+	}
+	var services ServiceLinks
+	err = json.Unmarshal(data, &services)
+	if err != nil {
+		return ServiceLinks{}, errors.New("Could not unmarshal services: " + err.Error())
+	}
+	return services, nil
 }
---
+++ backend/serviceLinks.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"net/http"
+
+	Coms "github.com/benjaminRoberts01375/Go-Communicate"
+)
+
+type ServiceLinks []ServiceLink
+
+type ServiceLink struct {
+	InternalAddress string `json:"internal_address"`
+	ExternalAddress string `json:"external_address"`
+}
+
+func (serviceLinks *ServiceLinks) Setup() {
+	diskServices, err := fileSystem.GetServices()
+	if err != nil {
+		Coms.PrintErrStr("Could not get services: " + err.Error())
+		return
+	}
+	*serviceLinks = diskServices
+}
+
+var serviceLinks = ServiceLinks{}
+
+func servicesSet(w http.ResponseWriter, r *http.Request) {
+	// Check JWT
+	_, newServiceLinks, err := checkUserRequest[ServiceLinks](r)
+	if err != nil {
+		Coms.PrintErrStr("Could not add service: " + err.Error())
+		Coms.ExternalPostRespondCode(http.StatusInternalServerError, w)
+		return
+	}
+	err = fileSystem.SetServices(*newServiceLinks)
+	if err != nil {
+		Coms.PrintErrStr("Could not set services in file system: " + err.Error())
+		Coms.ExternalPostRespondCode(http.StatusInternalServerError, w)
+		return
+	}
+	serviceLinks = *newServiceLinks
+}
