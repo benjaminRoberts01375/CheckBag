afa745ff99d7ecae1f4934a7635accffb5d0bfa1
benjaminroberts01375 <benroberts01375@gmail.com>
2025-07-12 18:09:16 -0400

    Enable requesting analytic data
    Only supports sending links (ie. internal and external)

---
+++ backend/analytics.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"net/http"
+
+	Coms "github.com/benjaminRoberts01375/Go-Communicate"
+)
+
+type ServiceData struct {
+	ServiceLink
+}
+
+func getServiceData(w http.ResponseWriter, r *http.Request) {
+	_, _, err := checkUserRequest[any](r)
+	if err != nil {
+		Coms.PrintErrStr("Could not verify user for analytic data: " + err.Error())
+		Coms.ExternalPostRespondCode(http.StatusInternalServerError, w)
+		return
+	}
+	requestedService := r.PathValue("service")
+	if requestedService == "" {
+		Coms.ExternalPostRespond(getAnalyticDashboard(), w)
+		return
+	} else {
+		Coms.ExternalPostRespond(getAnalyticData(), w)
+	}
+}
+
+func getAnalyticDashboard() []ServiceData {
+	var serviceData []ServiceData = make([]ServiceData, len(serviceLinks))
+	for i, service := range serviceLinks {
+		serviceData[i] = ServiceData{
+			ServiceLink: service,
+		}
+	}
+	return serviceData
+}
+
+func getAnalyticData() ServiceData {
+	return ServiceData{}
+}
