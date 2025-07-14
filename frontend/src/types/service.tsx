3f3cd86a46490ed1c9fdda9a09713e00f7f73dba
benjaminroberts01375 <benroberts01375@gmail.com>
2025-07-14 10:31:13 -0400

    Update service to be inline with backend + clientID

--- frontend/src/types/service.tsx
+++ frontend/src/types/service.tsx
@@ -1,10 +1,21 @@
 class Service {
 	internal_address: string;
-	external_address: string;
+	external_address: string[];
+	title: string;
+	id: string;
+	clientID: string;
 
-	constructor(internal_address: string, external_address: string) {
+	constructor(
+		internal_address: string,
+		external_address: string[],
+		title: string,
+		id: string = "",
+	) {
 		this.internal_address = internal_address;
 		this.external_address = external_address;
+		this.title = title;
+		this.id = id;
+		this.clientID = crypto.randomUUID();
 	}
 }
 
