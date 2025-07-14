41dc59c7edd508d89b929eaa8a2b055e10e3eba7
benjaminroberts01375 <benroberts01375@gmail.com>
2025-07-14 10:39:00 -0400

    Add services screen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import { ContextProvider } from "./context";
 import SignIn from "./screens/signin";
 import SignUp from "./screens/signup";
 import Dashboard from "./components/dashboard";
+import ServicesScreen from "./screens/services";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
@@ -16,7 +17,7 @@ createRoot(document.getElementById("root")!).render(
 					<Route path="/signup" element={<SignUp />} />
 					<Route path="/dashboard" element={<Dashboard />}>
 						<Route path="/dashboard/home" element={<h1>Home</h1>} />
-						<Route path="/dashboard/services" element={<h1>Services</h1>} />
+						<Route path="/dashboard/services" element={<ServicesScreen />} />
 					</Route>
 				</Routes>
 			</ContextProvider>
