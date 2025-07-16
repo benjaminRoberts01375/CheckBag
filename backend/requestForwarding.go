6067bea229ea2d61a77d668c87e8f93d594b2724
benjaminroberts01375 <benroberts01375@gmail.com>
2025-07-16 10:27:33 -0400

    Remove debugging

--- backend/requestForwarding.go
+++ backend/requestForwarding.go
@@ -4,26 +4,12 @@ import (
 	"bytes"
 	"io"
 	"net/http"
-	"strconv"
-	"time"
 
 	Coms "github.com/benjaminRoberts01375/Go-Communicate"
 )
 
-// Attempts act as a proxy server for external requests to internal services. Ex. dev.benlab.us -> 192.168.0.50:8154
+// Attempts act as a proxy server for external requests to internal services. Ex. dev.benlab.us/my/stuff -> 192.168.0.50:8154/my/stuff
 func requestForwarding(w http.ResponseWriter, r *http.Request) {
-	Coms.Println("Headers from client:")
-	for k, values := range r.Header {
-		for _, v := range values {
-			Coms.Println("→ Header: ", k, ": ", v)
-		}
-	}
-	Coms.Println("Other client requested info:")
-	Coms.Println("→ Host: " + r.Host)
-	Coms.Println("→ Request Type: " + r.Method)
-	Coms.Println("→ Path: " + r.URL.Path)
-	Coms.Println("→ Endpoint: " + r.PathValue("path"))
-	Coms.Println("→ Query: " + r.URL.RawQuery)
 	var bodyBytes []byte
 	var err error
 	if r.Body != nil {
@@ -34,8 +20,6 @@ func requestForwarding(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 	}
-	time.Sleep(time.Millisecond * 500)
-	// Send proxy request
 	requestedService := serviceLinks.GetServiceFromExternalURL(r.Host)
 	if requestedService == nil {
 		Coms.PrintErrStr("No service found for external URL: " + r.Host)
@@ -56,17 +40,12 @@ func requestForwarding(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Set headers for proxy
-	Coms.Println("Proxy headers to mealie")
 	for name, values := range r.Header {
 		for _, value := range values {
-			Coms.Println("→→ Header: " + name + ": " + value)
 			proxyRequest.Header.Add(name, value)
 		}
 	}
 
-	proxyRequest.Host = requestedService.InternalAddress
-
-	Coms.Println("Sending request")
 	client := &http.Client{}
 	proxyResponse, err := client.Do(proxyRequest)
 	if err != nil {
@@ -74,7 +53,6 @@ func requestForwarding(w http.ResponseWriter, r *http.Request) {
 		requestRespondCode(w, http.StatusInternalServerError)
 		return
 	}
-	Coms.Println("Got response")
 
 	// Respond to client
 
@@ -85,17 +63,13 @@ func requestForwarding(w http.ResponseWriter, r *http.Request) {
 		requestRespondCode(w, http.StatusInternalServerError)
 		return
 	}
-	Coms.Println("Response headers to client")
 	for name, values := range proxyResponse.Header {
 		for _, value := range values {
-			Coms.Println("← Response Header: " + name + ": " + value)
 			w.Header().Add(name, value)
 		}
 	}
 	w.WriteHeader(proxyResponse.StatusCode)
-	Coms.Println("← Status code: " + strconv.Itoa(proxyResponse.StatusCode))
 	w.Write(responseBytes)
-	Coms.Println("Done.")
 }
 
 func requestRespond(w http.ResponseWriter, data []byte, headers ...http.Header) error {
