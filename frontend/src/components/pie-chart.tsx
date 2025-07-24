34126d50f5b165fc6827d5b244be4732514701fc
benjaminroberts01375 <benroberts01375@gmail.com>
2025-07-24 12:58:07 -0400

    Pass theme to pie charts

--- frontend/src/components/pie-chart.tsx
+++ frontend/src/components/pie-chart.tsx
@@ -2,26 +2,30 @@ import "../styles.css";
 import graphsStyles from "./graphs.module.css";
 import ChartData from "../types/chart-data";
 import { PieChart } from "@mui/x-charts/PieChart";
+import { ThemeProvider, Theme } from "@mui/material/styles";
 
 interface PieChartProps {
 	data: ChartData[];
 	title: string;
+	theme: Theme;
 }
 
-const PieChartComponent = ({ data, title }: PieChartProps) => {
+const PieChartComponent = ({ data, title, theme }: PieChartProps) => {
 	return (
-		<div>
-			<h2 className={graphsStyles["header"]}>{title}</h2>
-			<PieChart
-				series={[
-					{
-						data: data,
-					},
-				]}
-				width={200}
-				height={200}
-			/>
-		</div>
+		<ThemeProvider theme={theme}>
+			<div id={graphsStyles["container"]}>
+				<h2 className={graphsStyles["header"]}>{title}</h2>
+				<PieChart
+					series={[
+						{
+							data: data,
+						},
+					]}
+					width={200}
+					height={200}
+				/>
+			</div>
+		</ThemeProvider>
 	);
 };
 
