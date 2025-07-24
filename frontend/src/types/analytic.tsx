4d6d6b2d2ad510d09c15eb55c7f3fa7a4cd25d8b
benjaminroberts01375 <benroberts01375@gmail.com>
2025-07-24 12:03:14 -0400

    Add response codes

--- frontend/src/types/analytic.tsx
+++ frontend/src/types/analytic.tsx
@@ -3,25 +3,34 @@ class Analytic {
 	country: Map<string, number>;
 	ip: Map<string, number>;
 	resource: Map<string, number>;
+	responseCode: Map<number, number>;
 
 	constructor(
 		quantity: number,
 		country: Map<string, number>,
 		ip: Map<string, number>,
 		resource: Map<string, number>,
+		responseCode: Map<number, number>,
 	) {
 		this.quantity = quantity;
 		this.country = country;
 		this.ip = ip;
 		this.resource = resource;
+		this.responseCode = responseCode;
 	}
 
 	static fromJSON(data: any): Analytic {
 		return new Analytic(
 			data.quantity,
-			new Map<string, number>(Object.entries(data.country || {})),
-			new Map<string, number>(Object.entries(data.ip || {})),
-			new Map<string, number>(Object.entries(data.resource || {})),
+			new Map<string, number>(Object.entries(data.country)),
+			new Map<string, number>(Object.entries(data.ip)),
+			new Map<string, number>(Object.entries(data.resource)),
+			new Map<number, number>(
+				Object.entries(data.response_code).map(([key, value]) => [
+					parseInt(key, 10),
+					value as number,
+				]),
+			),
 		);
 	}
 }
