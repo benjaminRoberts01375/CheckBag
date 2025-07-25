59c862b6b2fe8d08d1a65a18ccdf56964eb8b3c8
benjaminroberts01375 <benroberts01375@gmail.com>
2025-07-24 22:43:27 -0400

    Add the animated background to the services screen

--- frontend/src/screens/services.tsx
+++ frontend/src/screens/services.tsx
@@ -3,6 +3,7 @@ import servicesStyles from "./services.module.css";
 import { useList } from "../context-hook";
 import ServiceEdit from "../components/service-edit";
 import { useEffect } from "react";
+import AnimatedBackground from "../components/animated-background";
 
 const ServicesScreen = () => {
 	const { services, requestServiceData } = useList();
@@ -11,32 +12,35 @@ const ServicesScreen = () => {
 	}, []);
 
 	return (
-		<div id={servicesStyles["container"]}>
-			<table id={servicesStyles["fancy-table"]}>
-				<thead>
-					<tr>
-						<th>
-							<p>Name</p>
-						</th>
-						<th>
-							<p>External Address</p>
-						</th>
-						<th>
-							<p>Internal Address</p>
-						</th>
-						<th>
-							<p>Actions</p>
-						</th>
-					</tr>
-				</thead>
-				<tbody>
-					{services.map(service => (
-						<ServiceEdit service={service} key={service.clientID} />
-					))}
-					<ServiceEdit service={undefined} key={"new"} />
-				</tbody>
-			</table>
-		</div>
+		<>
+			<AnimatedBackground nodes={services.length} speed={0.5} />
+			<div id={servicesStyles["container"]}>
+				<table id={servicesStyles["fancy-table"]}>
+					<thead>
+						<tr>
+							<th>
+								<p>Name</p>
+							</th>
+							<th>
+								<p>External Address</p>
+							</th>
+							<th>
+								<p>Internal Address</p>
+							</th>
+							<th>
+								<p>Actions</p>
+							</th>
+						</tr>
+					</thead>
+					<tbody>
+						{services.map(service => (
+							<ServiceEdit service={service} key={service.clientID} />
+						))}
+						<ServiceEdit service={undefined} key={"new"} />
+					</tbody>
+				</table>
+			</div>
+		</>
 	);
 };
 
