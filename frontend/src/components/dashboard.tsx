f4bab2222c307d0483373b721429037c8ca2a7fe
benjaminroberts01375 <benroberts01375@gmail.com>
2025-07-25 16:21:37 -0400

    Move animated background into dashboard template

--- frontend/src/components/dashboard.tsx
+++ frontend/src/components/dashboard.tsx
@@ -2,15 +2,29 @@ import "../styles.css";
 import DashboardStyles from "./dashboard.module.css";
 import { Outlet } from "react-router-dom";
 import Navbar from "./navbar";
+import AnimatedBackground from "../components/animated-background";
+import { useList } from "../context-hook";
+import { useEffect, useState } from "react";
 
 const Dashboard = () => {
+	const { services } = useList();
+	const [nodes, setNodes] = useState(services.length);
+
+	useEffect(() => {
+		console.log("Updating nodes to " + services.length);
+		setNodes(services.length);
+	}, [services]);
+
 	return (
-		<div id={DashboardStyles["dashboard-container"]}>
-			<Navbar />
-			<div className={DashboardStyles["content-area"]}>
-				<Outlet />
+		<>
+			<AnimatedBackground nodes={nodes} speed={0.5} />
+			<div id={DashboardStyles["dashboard-container"]}>
+				<Navbar />
+				<div className={DashboardStyles["content-area"]}>
+					<Outlet />
+				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
