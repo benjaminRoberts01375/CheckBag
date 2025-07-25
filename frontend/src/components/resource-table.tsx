17a6bdab03f3b2d9a16e200c5ca5959a3f99bf0d
benjaminroberts01375 <benroberts01375@gmail.com>
2025-07-25 16:31:27 -0400

    Allow resource table to show "No data to display"

--- frontend/src/components/resource-table.tsx
+++ frontend/src/components/resource-table.tsx
@@ -12,38 +12,42 @@ const ResourceTable = ({ data, title }: ResourceTableProps) => {
 	return (
 		<div id={GraphStyles["container"]}>
 			<h2 className="header">{title}</h2>
-			<table className={ResourceTableStyles["styled-table"]}>
-				<thead>
-					<tr>
-						<th>
-							<p>Service</p>
-						</th>
-						<th>
-							<p>Resource</p>
-						</th>
-						<th>
-							<p>Quantity</p>
-						</th>
-					</tr>
-				</thead>
-				<tbody>
-					{data.map(resourceUsage => {
-						return (
-							<tr key={resourceUsage.service + resourceUsage.resource}>
-								<td>
-									<p>{resourceUsage.service}</p>
-								</td>
-								<td>
-									<p>{resourceUsage.resource}</p>
-								</td>
-								<td>
-									<p>{resourceUsage.quantity}</p>
-								</td>
-							</tr>
-						);
-					})}
-				</tbody>
-			</table>
+			{data.length === 0 ? (
+				<p className={ResourceTableStyles["no-data"]}>No data to display</p>
+			) : (
+				<table className={ResourceTableStyles["styled-table"]}>
+					<thead>
+						<tr>
+							<th>
+								<p>Service</p>
+							</th>
+							<th>
+								<p>Resource</p>
+							</th>
+							<th>
+								<p>Quantity</p>
+							</th>
+						</tr>
+					</thead>
+					<tbody>
+						{data.map(resourceUsage => {
+							return (
+								<tr key={resourceUsage.service + resourceUsage.resource}>
+									<td>
+										<p>{resourceUsage.service}</p>
+									</td>
+									<td>
+										<p>{resourceUsage.resource}</p>
+									</td>
+									<td>
+										<p>{resourceUsage.quantity}</p>
+									</td>
+								</tr>
+							);
+						})}
+					</tbody>
+				</table>
+			)}
 		</div>
 	);
 };
