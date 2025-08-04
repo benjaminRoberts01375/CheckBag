5e5214fb6bc5410855f3f5134c7f1c17d648ba3a
benjaminroberts01375 <benroberts01375@gmail.com>
2025-08-04 12:06:08 -0400

    Add ID tracking to pie charts
    This doesn't change the UI much if at all, but hey it's there

--- frontend/src/types/chart-data.tsx
+++ frontend/src/types/chart-data.tsx
@@ -1,10 +1,12 @@
 class ChartData {
 	value: number;
 	label: string;
+	id: string | number;
 
 	constructor(value: number, label: string) {
 		this.value = value;
 		this.label = label;
+		this.id = label;
 	}
 }
 
