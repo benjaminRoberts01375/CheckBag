509eb4b6ed71059174df9f6b288deae123bb4e3c
benjaminRoberts01375 <61424934+benjaminRoberts01375@users.noreply.github.com>
2025-08-09 16:05:49 -0400

    Remove "Hello, React" endpoint

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"; // Import Navigate
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { ContextProvider } from "./context";
 import SignIn from "./screens/signin";
 import SignUp from "./screens/signup";
@@ -13,7 +13,6 @@ createRoot(document.getElementById("root")!).render(
 		<BrowserRouter>
 			<ContextProvider>
 				<Routes>
-					<Route path="/" element={<h1>Hello, React!</h1>} />
 					<Route path="/signin" element={<SignIn />} />
 					<Route path="/signup" element={<SignUp />} />
 					<Route path="/dashboard" element={<Dashboard />}>
