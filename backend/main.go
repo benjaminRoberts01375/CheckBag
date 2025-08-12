93757b27817483b60b4d2ba13c0c4264b9eaa7ad
benjaminRoberts01375 <61424934+benjaminRoberts01375@users.noreply.github.com>
2025-08-12 12:00:23 -0400

    Disable userResetPassword endpoint

--- backend/main.go
+++ backend/main.go
@@ -31,13 +31,13 @@ func main() {
 }
 
 func setupEndpoints() {
-	http.HandleFunc("GET /api/user-exists", userExists)                 // Check if the user already exists
-	http.HandleFunc("POST /api/user-sign-up", newUser)                  // Sign up with username and password
-	http.HandleFunc("POST /api/user-sign-in", userSignIn)               // Sign in with username and password
-	http.HandleFunc("POST /api/user-sign-in-jwt", userJWTSignIn)        // Sign in with JWT
-	http.HandleFunc("POST /api/user-logout", userLogout)                // Invalidate the user's JWT
-	http.HandleFunc("POST /api/user-reset-password", userResetPassword) // Reset the user's password
-	http.HandleFunc("POST /api/services-set", servicesSet)              // Setting/replacing all services
-	http.HandleFunc("GET /api/service-data", getServiceData)            // Getting analytics
-	http.HandleFunc("/api/service/{path...}", requestForwarding)        // Proxying requests
+	http.HandleFunc("GET /api/user-exists", userExists)          // Check if the user already exists
+	http.HandleFunc("POST /api/user-sign-up", newUser)           // Sign up with username and password
+	http.HandleFunc("POST /api/user-sign-in", userSignIn)        // Sign in with username and password
+	http.HandleFunc("POST /api/user-sign-in-jwt", userJWTSignIn) // Sign in with JWT
+	http.HandleFunc("POST /api/user-logout", userLogout)         // Invalidate the user's JWT
+	// http.HandleFunc("POST /api/user-reset-password", userResetPassword) // Reset the user's password
+	http.HandleFunc("POST /api/services-set", servicesSet)       // Setting/replacing all services
+	http.HandleFunc("GET /api/service-data", getServiceData)     // Getting analytics
+	http.HandleFunc("/api/service/{path...}", requestForwarding) // Proxying requests
 }
