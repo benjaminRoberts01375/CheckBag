1f8989232ff69892b49a1de155067fd54fd4abfc
benjaminRoberts01375 <61424934+benjaminRoberts01375@users.noreply.github.com>
2025-08-15 09:02:50 -0400

    Comment out `userResetPassword`

--- backend/userResetPassword.go
+++ backend/userResetPassword.go
@@ -1,21 +1,17 @@
 package main
 
-import (
-	"net/http"
-)
-
 // Reset the user's password. Authentication is done with the JWT.
-func userResetPassword(w http.ResponseWriter, r *http.Request) {
-	_, password, err := checkUserRequest[string](r)
-	if err != nil {
-		requestRespondCode(w, http.StatusForbidden)
-		return
-	}
-	newPasswordHash, err := createPasswordHash(*password)
-	if err != nil {
-		requestRespondCode(w, http.StatusInternalServerError)
-		return
-	}
-	fileSystem.SetUserData(string(newPasswordHash))
-	requestRespondCode(w, http.StatusOK)
-}
+// func userResetPassword(w http.ResponseWriter, r *http.Request) {
+// 	_, password, err := checkUserRequest[string](r)
+// 	if err != nil {
+// 		requestRespondCode(w, http.StatusForbidden)
+// 		return
+// 	}
+// 	newPasswordHash, err := createPasswordHash(*password)
+// 	if err != nil {
+// 		requestRespondCode(w, http.StatusInternalServerError)
+// 		return
+// 	}
+// 	fileSystem.SetUserData(string(newPasswordHash))
+// 	requestRespondCode(w, http.StatusOK)
+// }
