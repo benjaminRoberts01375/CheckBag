361a6467218be63139ff1bb2e73f9e59501ba6cc
benjaminRoberts01375 <61424934+benjaminRoberts01375@users.noreply.github.com>
2025-09-30 17:04:46 -0400

    Initial UI for editing services

--- frontend/src/screens/services.tsx
+++ frontend/src/screens/services.tsx
@@ -2,7 +2,7 @@ import "../styles.css";
 import ServicesStyles from "./services.module.css";
 import DashboardStyles from "./dashboard.module.css";
 import { useList } from "../context-hook";
-import { useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import Service from "../types/service.tsx";
 import { CgMoreVerticalAlt } from "react-icons/cg";
 
@@ -76,9 +76,41 @@ const ServiceStatus = ({ address }: ServiceURLProps) => {
 };
 
 const EditService = ({ service }: ServiceListEntryProps) => {
+	const [title, setTitle] = useState(service.title || "Untitled Service");
+	const [incomingAddress, setIncomingAddress] = useState(service.external_address[0] || "");
+	const [outgoingAddress, setOutgoingAddress] = useState(service.internal_address || "");
+
 	return (
-		<div>
-			<h1>Edit Service</h1>
-		</div>
+		<form id={ServicesStyles["edit-service-container"]}>
+			<h1>Editing "{title}"</h1>
+			<input
+				type="text"
+				placeholder="Service Name"
+				value={title}
+				onChange={e => setTitle(e.target.value)}
+				className={ServicesStyles["input"]}
+			/>
+			<p>From:</p>
+			<input
+				type="text"
+				placeholder="Forward Address"
+				value={incomingAddress}
+				onChange={e => setIncomingAddress(e.target.value)}
+				className={ServicesStyles["input"]}
+			/>
+			<p>To:</p>
+			<input
+				type="text"
+				placeholder="Forward Address"
+				value={outgoingAddress}
+				onChange={e => setOutgoingAddress(e.target.value)}
+				className={ServicesStyles["input"]}
+			/>
+			<div id={ServicesStyles["submission-buttons"]}>
+				<button className="delete">Delete</button>
+				<button className="cancel">Cancel</button>
+				<button className="submit">Submit</button>
+			</div>
+		</form>
 	);
 };
