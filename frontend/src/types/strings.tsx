fe02851090086661d253be7be5474dc6e8a45c85
benjaminroberts01375 <benroberts01375@gmail.com>
2025-10-06 16:23:01 +0100

    Add supported protocols type

--- frontend/src/types/strings.tsx
+++ frontend/src/types/strings.tsx
@@ -1,3 +1,4 @@
 export const Timescales = ["hour", "day", "month", "year"] as const;
 export type Timescale = (typeof Timescales)[number];
 export type CookieKeys = "session-token";
+export type CommunicationProtocols = "http" | "https";
