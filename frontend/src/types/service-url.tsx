648e84e000dccfd6506f710090c815847b6655eb
benjaminRoberts01375 <61424934+benjaminRoberts01375@users.noreply.github.com>
2025-10-09 11:03:33 -0400

    Create service-url.tsx

---
+++ frontend/src/types/service-url.tsx
@@ -0,0 +1,21 @@
+import { CommunicationProtocol } from "./strings";
+
+class ServiceURL {
+	protocol: CommunicationProtocol;
+	domain: string;
+	port: number;
+
+	constructor(protocol: CommunicationProtocol = "http", domain: string = "", port: number = 80) {
+		this.protocol = protocol;
+		this.domain = domain;
+		this.port = port;
+		if (this.port === 0) {
+			this.port = this.protocol === "http" ? 80 : 443;
+		}
+	}
+	toString(): string {
+		return `${this.protocol}://${this.domain}:${this.port}`;
+	}
+}
+
+export default ServiceURL;
