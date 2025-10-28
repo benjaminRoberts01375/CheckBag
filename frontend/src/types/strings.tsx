9b8c439dc7cd4a551f5dec7337cfe5fff7a68f42
benjaminRoberts01375 <61424934+benjaminRoberts01375@users.noreply.github.com>
2025-10-28 14:59:01 -0400

    Use correct cookie name

--- frontend/src/types/strings.tsx
+++ frontend/src/types/strings.tsx
@@ -1,5 +1,5 @@
 export const Timescales = ["hour", "day", "month", "year"] as const;
 export type Timescale = (typeof Timescales)[number];
-export type CookieKeys = "session-token";
+export type CookieKeys = "checkbag-session-token";
 export const CommunicationProtocols = ["http", "https"];
 export type CommunicationProtocol = (typeof CommunicationProtocols)[number];
